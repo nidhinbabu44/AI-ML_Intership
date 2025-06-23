a557dafe3ab4ec3ee1a06cc7c618c3d6094c5d59
nidhinbabu44 <nidhinbabu44@gmail.com>
2025-06-23 11:49:25 +0300

    KNN_algr

--- ml_predict.py
+++ ml_predict.py
@@ -1,7 +1,19 @@
 import joblib
 import numpy as np
+import pandas as pd
 
-pred_value = np.array ([[160]])
-result_model = joblib.load("my_model.pkl")
-result = result_model.predict(pred_value)
-print(result)
+
+df = pd.read_csv("data.csv")
+
+X= df[["height"]]
+Y = df[["weight"]]
+
+pred_value = pd.DataFrame ([[160]], columns=X.columns)
+result_model_lr = joblib.load("lr_model.pkl")
+result_model_knn = joblib.load("knn_model.pkl")
+
+result_lr = result_model_lr.predict(pred_value)
+print(result_lr)
+
+result_knn = result_model_knn.predict(pred_value)
+print(result_knn)
--- ml_program.py
+++ ml_program.py
@@ -4,6 +4,8 @@ from sklearn import linear_model
 import numpy as np
 from sklearn.model_selection import train_test_split
 import joblib
+from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.neighbors import KNeighborsRegressor as kn
 
 df = pd.read_csv("data.csv")
 
@@ -15,14 +17,34 @@ mt.xlabel("Height in CM")
 mt.ylabel("Weight in KG")
 mt.show()
 
-X_train,X_text,Y_train,Ytest = train_test_split(X,Y, test_size=0.2,random_state=42)
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y, test_size=0.2,random_state=42)
 print(X_train)
 
 # Training Start
 lr_model = linear_model.LinearRegression()
 lr_model.fit(X_train,Y_train)
+
+knn_model = kn(n_neighbors=3)
+knn_model.fit(X_train,Y_train)
+
 # Training Over----
 
-joblib.dump(lr_model,"my_model.pkl")
+joblib.dump(lr_model,"lr_model.pkl")
+joblib.dump(knn_model,"knn_model.pkl")
 
-print("Training Completed")
+print("Training Completed...!")
+
+# Evaluation
+
+prediction_result_weight = lr_model.predict(X_test)
+
+print("LR MSE",mean_squared_error(Y_test,prediction_result_weight))
+
+print("LR RMSE",np.sqrt(mean_squared_error(Y_test,prediction_result_weight)))
+print("LR r2 square", r2_score(Y_test,prediction_result_weight))
+
+# Evaluation for knn
+prediction_result_weight_knn = knn_model.predict(X_test)
+print("KNN MSE",mean_squared_error(Y_test,prediction_result_weight_knn))
+print("KNN RMSE",np.sqrt(mean_squared_error(Y_test,prediction_result_weight_knn)))
+print("KNN r2 square", r2_score(Y_test,prediction_result_weight_knn))
